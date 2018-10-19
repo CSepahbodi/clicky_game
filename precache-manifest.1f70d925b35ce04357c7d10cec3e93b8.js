eefd536e8d13e4b2343ede8e1173adef972956d5
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-10-18 21:10:37 -0700

    Updates

---
+++ precache-manifest.1f70d925b35ce04357c7d10cec3e93b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd845ee937b9e5f2a5a2",
+    "url": "/clicky_game/static/js/runtime~main.bd845ee9.js"
+  },
+  {
+    "revision": "ec6e29782271394498a0",
+    "url": "/clicky_game/static/js/main.ec6e2978.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/clicky_game/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "ec6e29782271394498a0",
+    "url": "/clicky_game/static/css/main.670861aa.chunk.css"
+  },
+  {
+    "revision": "9baf5c58b4fe4874cf8a1f7a9ab8d6bc",
+    "url": "/clicky_game/index.html"
+  }
+];
